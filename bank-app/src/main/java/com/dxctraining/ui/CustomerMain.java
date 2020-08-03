b6fe9da4dea5f571ae5400063a29ad5cb32a848a
fazilshaik <shaikfazil1.sf@gmail.com>
2020-08-04 02:50:14 +0530

    assignment

--- bank-app/src/main/java/com/dxctraining/ui/CustomerMain.java
+++ bank-app/src/main/java/com/dxctraining/ui/CustomerMain.java
@@ -5,7 +5,7 @@ import com.dxctraining.*;
 
 public class CustomerMain {
 	private Map<String, Customer> store = new HashMap<>();
-	
+
 	public static void main(String args[]) {
 		CustomerMain demo = new CustomerMain();
 		demo.runApp();
@@ -13,38 +13,38 @@ public class CustomerMain {
 
 	private void runApp() {
 		try {
-			Account account1= new Account(500, "pan001");
-			Account account2= new Account(1000,"pan002");
-			Account account3= new Account(1500,"pan003");
-			Account account4= new Account(2000,"pan004");
-			final String name1= "Sravya", name2= "Harsha", name3= "Fazil", name4= "Bubbly";
-			RegularCustomer customer1 = new RegularCustomer("id1", name1, account1,"Hyderabad");
+			Account account1 = new Account(500, "pan001");
+			Account account2 = new Account(1000, "pan002");
+			Account account3 = new Account(1500, "pan003");
+			Account account4 = new Account(2000, "pan004");
+			final String name1 = "Sravya", name2 = "Harsha", name3 = "Fazil", name4 = "Bubbly";
+			RegularCustomer customer1 = new RegularCustomer("id1", name1, account1, "Hyderabad");
 			customer1.setAccount(account1);
-			BussinessCustomer customer2 = new BussinessCustomer("id2", name2, account2,"Kurnool");
+			BussinessCustomer customer2 = new BussinessCustomer("id2", name2, account2, "Kurnool");
 			customer2.setAccount(account2);
-			RegularCustomer customer3 = new RegularCustomer("id3", name3, account3,"Bangalore");
+			RegularCustomer customer3 = new RegularCustomer("id3", name3, account3, "Bangalore");
 			customer3.setAccount(account3);
-			BussinessCustomer customer4 = new BussinessCustomer("id4", name4, account4,"Chennai");
+			BussinessCustomer customer4 = new BussinessCustomer("id4", name4, account4, "Chennai");
 			customer4.setAccount(account4);
-			
+
 			store.put(customer1.getId(), customer1);
 			store.put(customer2.getId(), customer2);
 			store.put(customer3.getId(), customer3);
 			store.put(customer4.getId(), customer4);
-			
+
 			System.out.println("*****Displaying all the customers*****");
 			displayAll();
-		}catch (InvalidCustomerArgumentException e) {
+		} catch (InvalidCustomerArgumentException e) {
 			System.out.println("Customer is null");
-		}catch (CustomerNotFoundException e) {
+		} catch (CustomerNotFoundException e) {
 			System.out.println("Customer not found");
 		}
-		
+
 	}
 
 	public void displayAll() {
 		Set<String> keys = store.keySet();
-		for(String key:keys) {
+		for (String key : keys) {
 			Customer current = store.get(key);
 			boolean isRegCust = current instanceof RegularCustomer;
 			if (isRegCust) {
@@ -55,33 +55,30 @@ public class CustomerMain {
 			if (isBussCust) {
 				BussinessCustomer bc = (BussinessCustomer) current;
 				display(bc);
+			}
+
 		}
-		
+
 	}
-
-}
-
 
 	public void display(RegularCustomer customer) {
 		displayCustomer(customer);
-		System.out.println("Home Address: "+customer.getHomeAddress());
-		
+		System.out.println("Home Address: " + customer.getHomeAddress());
+
 	}
+
 	public void display(BussinessCustomer customer) {
 		displayCustomer(customer);
-		System.out.println("Bussiness Address: "+customer.getBussinessAddress());
-		
+		System.out.println("Bussiness Address: " + customer.getBussinessAddress());
+
 	}
 
 	public void displayCustomer(Customer customer) {
 		String id = customer.getId();
 		String name = customer.getName();
-		System.out.println("Id is "+id+" Name is "+name);
+		System.out.println("Id is " + id + " Name is " + name);
 		Account account = customer.getAccount();
-		System.out.println("Balance is "+account.getBalance()+" Pancard number is "+account.getPancard());
-	}
-		
-		
+		System.out.println("Balance is " + account.getBalance() + " Pancard number is " + account.getPancard());
 	}
 
-
+}
